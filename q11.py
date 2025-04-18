2bcc849af62f1d4aeca45364b04eaabd29c4ea42
Anusha S Reddy <4q4947@gmail.com>
2025-04-18 22:00:55 +0530

    Celsius to Fahrenheit

---
+++ q11.py
@@ -0,0 +1,5 @@
+#Converting Celsius to Fahrenheit
+celsius=float(input("Enter the temperature in celsius: "))
+    # Fahrenheit = (celsius *  9/5) + 32
+Fahrenheit = (celsius *  9/5) + 32
+print(f"{celsius} degrees celsius is equal to {Fahrenheit} degrees Fahrenheit")
