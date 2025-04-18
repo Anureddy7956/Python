e5778fdef0135d58eef0317318ebd54180119c13
Anusha S Reddy <4q4947@gmail.com>
2025-04-18 21:48:38 +0530

    Conversion of Kilometers to Miles

---
+++ q10.py
@@ -0,0 +1,5 @@
+#Converting kilometers to miles	
+kilo_meters= float(input("Enter the distance in kilometers: "))
+# 1 kilometer =0.621 miles
+miles = kilo_meters * 0.621
+print(f"{kilo_meters} Kilometers is equal to {miles} miles")
